54d995110c1f66e57ce0e7da225ce6af48287852
Karen Gonçalves <karengoncalves128@gmail.com>
2024-04-07 19:18:42 -0300

    CodingBat7

--- Exercicios/CodingBat7/CodingBat7.py
+++ Exercicios/CodingBat7/CodingBat7.py
@@ -64,7 +64,7 @@ def extra_end(s):
 # first_half('HelloThere') -> 'Hello'
 # first_half('abcdef') -> 'abc'
 def first_half(s):
-  return 
+  return s[:int(len(s)/2)]
 
 # I. sem_pontas
 # seja uma string s de pelo menos dois caracteres
@@ -73,7 +73,7 @@ def first_half(s):
 # without_end('python') -> 'ytho'
 # without_end('coding') -> 'odin'
 def sem_pontas(s):
-  return 
+  return s[1:len(s)-1]
 
 # J. roda2
 # rodar uma string s duas posições
@@ -81,7 +81,7 @@ def sem_pontas(s):
 # left2('Hello') -> 'lloHe'
 # left2('Hi') -> 'Hi'
 def roda2(s):
-  return 
+  return s[2:] + s[:2]
 
 
 # Provided simple test() function used in main() to print
