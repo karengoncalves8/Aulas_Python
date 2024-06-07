4033b57cb48bdefbacca53a2b6d1b4b85e29b6f2
karengoncalves8 <karengoncalves128@gmail.com>
2024-06-07 08:52:28 -0300

    extra anotações

--- Exercicios/Extra/Enade2011.py
+++ Exercicios/Extra/Enade2011.py
@@ -2,10 +2,27 @@
 # 1 3 7 15 
 # 1 2 3 4
 
-graos = 0
-an = 1
-for i in range(1, 4):
-    graos += an * 2
-    an = graos 
+# graos = 0
+# an = 1
+# for i in range(1, 4):
+#     graos += an * 2
+#     an = graos 
 
-print(graos)
+# print(graos)
+
+def Josephus(n, m):
+    circulo = [p for p in range(1, n+1)]
+    while circulo != 1:
+        for p in range(0, len(circulo), m):
+            circulo.remove(circulo[p])
+
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+  
+test(Josephus(50, 3), 11)
+
